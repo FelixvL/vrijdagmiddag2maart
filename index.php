2585f86ca5376332005b777ccbd71e5788609257
Felixvl <digitaleservice@gmail.com>
2018-03-02 14:28:38 +0100

    dropdown werkt

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<select>
+  <option value="volvo">Volvo</option>
+  <option value="saab">Saab</option>
+  <option value="opel">Opel</option>
+  <option value="audi">Audi</option>
+</select>
+
+
+
 <script>
  function go(){
     var voorbeeld = document.getElementById("deid").value;
@@ -6,44 +15,49 @@
 }
 </script>
 <?php
+$debroodjes = allebroodjes();
+
 if(isset($_GET['input'])){
     echo ">>".$_GET['input'];
 }
-echo "<input type=text id=deid ><input type=button onclick=go()>";
+bestelMenu($debroodjes);
+//echo "<input type=text id=deid >";
+echo "<input type=button onclick=go() value=bestellen>";
 echo "<br><br>";
-$debroodjes = allebroodjes();
 productenTonen($debroodjes);
 
-
-//brie
-//gehakt
-//gezond
-//plofkipkerrie
+function bestelMenu($alledingen){
+    echo "<select id=deid>";
+    foreach($alledingen as $ding){
+        echo "<option value=\"$ding\">$ding</option>";
+    }
+    echo "</select>";
+}
 
 function productenTonen($lijstProducten){
     foreach($lijstProducten as $product){
-        echo zoekAfbeelding($product);
+        echo "<img src=afbeelding/".zoekAfbeelding($product)." width=200><br/>";
         echo "$product <br>";
     }
 }
 function allebroodjes(){
-    $broodjes = ["Broodje Gezond","Broodje Bal","Broodje Brie","Broodje KipKerrie"];
+    $broodjes = ["Broodje Gezond","Broodje Bal","Broodje Brie","Broodje PlofKipKerrie"];
     return $broodjes;
 }
 function zoekAfbeelding($broodje){
     switch($broodje){
 //        case allebroodjes()[0]:
         case "Broodje Gezond":
-            return "<img src=afbeelding/gezond.jpg width=200><br/>";
+            return "gezond.jpg";
             break;
         case allebroodjes()[1]:
-            return "<img src=afbeelding/gehakt.jpg width=200><br/>";
+            return "gehakt.jpg";
             break;
         case allebroodjes()[2]:
-            return "<img src=afbeelding/brie.jpg width=200><br/>";
+            return "brie.jpg";
             break;
         case allebroodjes()[3]:
-            return "<img src=afbeelding/plofkipkerrie.jpg width=200><br/>";
+            return "plofkipkerrie.jpg";
             break;
     }
 }
