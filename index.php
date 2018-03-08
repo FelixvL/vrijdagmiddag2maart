9e95465219fcb41a0d29613b3f873b82531cc987
Felixvl <digitaleservice@gmail.com>
2018-03-08 14:00:41 +0100

    De eerste dieren worden gevoerd

--- index.php
+++ index.php
@@ -1,3 +1,36 @@
 <?php
+$kip = new Kip();
+$varken = new Varken();
+$mais = new Mais();
 
-class Kinderboerderij{}
+$kinderboerderij = new Kinderboerderij();
+$kinderboerderij->voerronde($kip);
+//$kinderboerderij->voerronde($mais);
+$kinderboerderij->voerronde($varken);
+
+//$kip->eten();
+//$kip->naam = "Dagobert";
+//echo $kip->naam;
+
+
+class Kinderboerderij{
+    public function voerronde(Dier $dier){
+        echo "We doen een voerronde";
+    }
+}
+class Mais{}
+abstract class Dier{
+    public $naam;
+    public $dieet;
+    public $carnivoor;
+    public function eten(){
+        echo "eten ".$this->naam;
+    }
+
+    
+}
+class Kip extends Dier{    
+}
+
+class Varken extends Dier{    
+}
